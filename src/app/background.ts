8bb5eba862c195c564684e8f890b6aff0a5b46dc
Jerad Rutnam <jeradrutnam@outlook.com>
2022-03-29 22:12:02 +0530

    Add API call sample from background

--- src/app/background.ts
+++ src/app/background.ts
@@ -7,23 +7,46 @@ chrome.tabs.onActivated.addListener(tab => {
 })
 
 const mockAPIEndpoint1 = () => {
-    return '{ "userID": "4546sdfdsf58qwfsa", "userName": "John Does" }';
+    return '{ "userID": "UUID3345526888", "userName": "John Does" }';
 }
 
 const mockAPIEndpoint2 = () => {
     return '{ "appName": "APP789660031", "appName": "My React App" }';
 }
 
+const responseStatus = (response) => {
+    if (response.status >= 200 && response.status < 300) {
+        return Promise.resolve(response)
+    } else {
+        return Promise.reject(new Error(response.statusText))
+    }
+}
+
+const json = (response) => {
+    return response.json()
+}
+
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    let response;
-    
     if (request.url == "http://api.webservice.com/getUser") {
-        response = mockAPIEndpoint1();
+        sendResponse({ message: mockAPIEndpoint1(), originalRequestURL: request.url, instanceID: request.instanceID });
     }
 
     if (request.url == "http://api.webservice.com/getApp") {
-        response = mockAPIEndpoint2();
+        setTimeout(() => {
+            sendResponse({ message: mockAPIEndpoint2(), originalRequestURL: request.url, instanceID: request.instanceID });
+        }, 500);
     }
 
-    sendResponse({ message: response, originalRequestURL: request.url, instanceID: request.instanceID });
+    if (request.url == "https://api.publicapis.org/entries") {
+        fetch("https://api.publicapis.org/entries")
+            .then(responseStatus)
+            .then(json)
+            .then((data) => {
+                sendResponse({ status: "success", message: data, originalRequestURL: request.url, instanceID: request.instanceID });
+            }).catch((error) => {
+                sendResponse({ status: "failed", message: error, originalRequestURL: request.url, instanceID: request.instanceID });
+            });
+    }
+
+    return true;
 });
